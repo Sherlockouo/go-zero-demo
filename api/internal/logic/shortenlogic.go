0ec66f2e158a5eb66a7e787f12b30451fb904a07
吴顶峰 <wudingfeng@msh.team>
2024-07-20 15:37:43 +0800

    feat: init gozerodemo

---
+++ api/internal/logic/shortenlogic.go
@@ -0,0 +1,39 @@
+package logic
+
+import (
+	"context"
+	"shorturl/api/internal/svc"
+	"shorturl/api/internal/types"
+	"shorturl/rpc/transform/transformer"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type ShortenLogic struct {
+	logx.Logger
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+}
+
+func NewShortenLogic(ctx context.Context, svcCtx *svc.ServiceContext) *ShortenLogic {
+	return &ShortenLogic{
+		Logger: logx.WithContext(ctx),
+		ctx:    ctx,
+		svcCtx: svcCtx,
+	}
+}
+
+func (l *ShortenLogic) Shorten(req *types.ShortenReq) (*types.ShortenResp, error) {
+	// 手动代码开始
+	rpcResp, err := l.svcCtx.Transformer.Shorten(l.ctx, &transformer.ShortenReq{
+		Url: req.Url,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return &types.ShortenResp{
+		Shorten: rpcResp.Shorten,
+	}, nil
+	// 手动代码结束
+}
